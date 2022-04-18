d368587d0c7b7f02f287741df9885756681a26d4
JagtaPawan <57706383+JagtaPawan@users.noreply.github.com>
2022-04-18 19:45:35 +0530

    Adding rest of the functionalities.
    Logging to Microsoft 365 Defender Portal
    Navigating to desired tabs
    Generating report
    Downloading report.

--- Defender Anti Virus Data Automation/code1.py
+++ Defender Anti Virus Data Automation/code1.py
@@ -5,3 +5,54 @@ from selenium import webdriver
 import time
 import pyautogui
 import datetime
+
+path = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(path)
+
+x = datetime.datetime.now()
+
+today = x.strftime("%A-%d-%m-%Y")
+
+driver.maximize_window()
+
+USERNAME = ""
+PASSWORD = ""
+
+url= "https://security.microsoft.com/machines"
+
+driver.get(url)
+
+time.sleep(10)
+
+user_name = driver.find_element_by_xpath('//*[@id="i0116"]')
+user_name.send_keys(USERNAME)
+
+submit_button = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+submit_button.click()
+
+time.sleep(10)
+
+password = driver.find_element_by_xpath('//*[@id="i0118"]')
+password.send_keys(PASSWORD)
+
+submit_button = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+submit_button.click()
+
+time.sleep(20)
+Yes = driver.find_element_by_xpath('//*[@id="idSIButton9"]')
+Yes.click()
+
+time.sleep(20)
+dropdown = driver.find_element_by_xpath('//*[@id="button_fancy-select-0"]/wcd-shared-icon[2]/icon-switch/fab-icon/i')
+dropdown.click()
+
+time.sleep(5)
+oneday = driver.find_element_by_xpath('/html/body/section[3]/ul/li[1]/div')
+oneday.click()
+
+time.sleep(5)
+export = driver.find_element_by_xpath('//*[@id="command-bar-item-1"]/wcd-shared-icon/icon-switch/fab-icon/i')
+export.click() 
+
+time.sleep(20)
+driver.quit()
