eb05735112fbbcbef6215111e5d743d83a32eca6
JagtaPawan <57706383+JagtaPawan@users.noreply.github.com>
2022-04-18 20:28:57 +0530

    #Neccesaary Imports
    Importing packages required for performing tasks.

---
+++ Defender Anti Virus Data Automation/code3.py
@@ -0,0 +1,9 @@
+## Neccesaary Imports
+
+from ctypes import alignment
+from itertools import count
+from turtle import left, right
+from numpy import left_shift, pi
+import pandas as pd
+import datetime
+import win32com.client as win32 #pip3 install openpyxl
