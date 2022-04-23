01492917ad140790db0cc2f358b754d1c6289ab1
Pawan Jagtap <57706383+JagtaPawan@users.noreply.github.com>
2022-04-23 15:50:18 +0530

    Adding the logic for file extension conversion
    Since we cannot save changes to .csv file, we need to save the file as .xlsx extension.

--- Advanced Hunting Query Automation/code2.py
+++ Advanced Hunting Query Automation/code2.py
@@ -7,3 +7,16 @@ x = datetime.datetime.now()
 
 today = x.strftime("%A-%d-%m-%Y")
 print(today)
+
+folder_path = r'C:\\Users\\jagtapawan\\Downloads\\'
+file_type = '\*csv'
+files = glob.glob(folder_path + file_type)
+latest_file = max(files, key=os.path.getctime)
+
+print (latest_file)
+
+old_name = latest_file
+new_name = "C:\\Users\\jagtapawan\\Downloads\\ADH"+today+".csv"
+
+os.rename(old_name,new_name)
+merge_all_to_a_book(glob.glob("C:\\Users\\jagtapawan\\Downloads\\ADH"+today+".csv"), "C:\\Users\\jagtapawan\\Downloads\\ADH"+today+".xlsx")
