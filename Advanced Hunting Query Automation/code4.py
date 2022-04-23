f2a61062232069d740ca9704b66dafb1f4341bec
Pawan Jagtap <57706383+JagtaPawan@users.noreply.github.com>
2022-04-23 15:55:42 +0530

    Clubbing code1,code2,code3
    Clubbing code1, code2, code3 all into one.

---
+++ Advanced Hunting Query Automation/code4.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\Automation\\ &python adh.py")
+time.sleep(10)
+
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\Automation\\ &python adh_code2prod.py")
+time.sleep(10)
+
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\Automation\\ &python adh_code3prod.py")
+time.sleep(10)
