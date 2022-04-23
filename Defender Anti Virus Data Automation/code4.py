99e8b3158b68175a4e7d323884a6c8e771bda4db
Pawan Jagtap <57706383+JagtaPawan@users.noreply.github.com>
2022-04-23 15:57:02 +0530

    Removing unwanted clutter

--- Defender Anti Virus Data Automation/code4.py
+++ Defender Anti Virus Data Automation/code4.py
@@ -1,11 +1,11 @@
 import os
 import time
 
-os.system("start cmd /k cd C:\\Users\\jagtapawan\\OneDrive - Accenture\\Wella\\Automation\\ &python code1.py")
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\ &python code1.py")
 time.sleep(10)
 
-os.system("start cmd /k cd C:\\Users\\jagtapawan\\OneDrive - Accenture\\Wella\\Automation\\ &python code2.py")
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\ &python code2.py")
 time.sleep(10)
 
-os.system("start cmd /k cd C:\\Users\\jagtapawan\\OneDrive - Accenture\\Wella\\Automation\\ &python code3.py")
+os.system("start cmd /k cd C:\\Users\\jagtapawan\\ &python code3.py")
 time.sleep(10)
