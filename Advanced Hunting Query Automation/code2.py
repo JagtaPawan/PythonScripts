87b43b37de303d0b8670ccbc5019d4a7a1af967b
Pawan Jagtap <57706383+JagtaPawan@users.noreply.github.com>
2022-04-23 15:48:05 +0530

    Fetching today's date
    Fetching today's date variable.

--- Advanced Hunting Query Automation/code2.py
+++ Advanced Hunting Query Automation/code2.py
@@ -4,3 +4,6 @@ import glob
 import datetime
 from pyexcel.cookbook import merge_all_to_a_book #pip3 install pyexcel pyexcel-xlsx
 x = datetime.datetime.now()
+
+today = x.strftime("%A-%d-%m-%Y")
+print(today)
